497d5627e93aa27319acc0cdc346874fa9a34fd8
root <root@msr-ufa-ap1.onyma.ttk.ru>
2016-04-20 14:02:48 +0500

    Only one instace control has been added (writting and cheking pid file)

--- authLog2Queue.sh
+++ authLog2Queue.sh
@@ -1,7 +1,14 @@
 current_dir=$(pwd)
 SCRIPT_PATH=`dirname "$0"`
 cd  $SCRIPT_PATH
-
-echo $! > pid_file
-./utils/infAuthFlow.sh | ./utils/flow2inMes.php auth |./utils/inMes2outMes.php |./utils//sendToQueue.php 
+file="./pid_file"
+if [ -f "$file" ]
+then
+pid=$(<$file)
+    echo "$file $var process already run."
+else 
+    echo $! > $file
+    ./utils/infAuthFlow.sh | ./utils/flow2inMes.php auth |./utils/inMes2outMes.php |./utils//sendToQueue.php 
+    rm $file
+fi
 cd $current_dir
